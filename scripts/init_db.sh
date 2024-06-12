89d2809b973b9748a72fe9f7e06668e892fb426c
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2024-06-13 02:43:34 +0530

    feat: shifted init script from dbgen to scripts

---
+++ scripts/init_db.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo $(date +"%s") > db/starttime
+
+echo "Initializing local databases"
+
+mkdir -p db
+[ -f db/local.db ] && rm db/local.db
+touch db/local.db
+
+sqlite3 db/local.db <<EOF
+CREATE TABLE repos (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE packages (
+    name TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    base TEXT,
+    version TEXT NOT NULL,
+    description TEXT,
+    groups TEXT,
+    compressed_size INTEGER,
+    installed_size INTEGER,
+    md5_sum TEXT,
+    sha256_sum TEXT,
+    pgp_signature TEXT,
+    home_url TEXT,
+    license TEXT,
+    arch TEXT,
+    build_date DATE,
+    packager TEXT,
+    replaces TEXT,
+    conflicts TEXT,
+    provides TEXT,
+    repo TEXT NOT NULL,
+    FOREIGN KEY(repo) REFERENCES repos(name)
+);
+EOF
+
+echo "Completed initializing local databases"
+
+echo $(date +"%s") > db/endtime
