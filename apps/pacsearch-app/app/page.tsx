f55d98d0c7e54f391fca9aaed875c8c639c0edbc
bravo68web <jbandyopadhayaya@gmail.com>
2024-07-01 06:58:02 +0000

    ui: added back links

--- apps/pacsearch-app/app/page.tsx
+++ apps/pacsearch-app/app/page.tsx
@@ -3,7 +3,7 @@ import PackageList from "@/components/PackageList";
 import Pagination from "@/components/Pagination";
 import RepoList from "@/components/RepoList";
 import SearchBar from "@/components/SearchBar";
-import { Github } from "lucide-react";
+import { SiGithub, SiArchlinux, SiWikipedia } from "@icons-pack/react-simple-icons";
 import Link from "next/link";
 
 export default function Home() {
@@ -12,15 +12,29 @@ export default function Home() {
             <div className="font-sans h-screen w-screen overflow-hidden text-rp-moon-text">
                 <div className="flex w-full">
                     <div className="flex w-1/6 h-screen bg-rp-moon-surface flex-col items-center justify-items-center">
-                        <div className="logo w-full bg-rp-moon-overlay">
-                            <h1 className="items-center justify-items-center text-2xl p-5">
-                                Repo List
-                            </h1>
+                        <div className="flex logo items-center justify-center w-full bg-rp-moon-overlay">
+                            <img 
+                                src="https://pacsearch-assets.b68.dev/logov2.png"
+                                width={225}
+                                height={225}
+                                alt="PacSearch"
+                            />
                         </div>
-                        <div className="logos my-4 mb-10 w-full px-4 flex gap-5 text-white">
-                            <Link className="p-4 bg-rp-moon-overlay rounded-full" href={"#"}>
-                                <Github className="h-6 w-6" />
+                        <div className="logos my-4 mb-10 w-full px-4 flex gap-5 text-white justify-center">
+                            <Link className="p-4 bg-rp-moon-overlay rounded-full" href={"https://github.com/BRAVO68WEB/pacsearch/"}>
+                                <SiGithub className="h-6 w-6" />
                             </Link>
+                            <Link className="p-4 bg-rp-moon-overlay rounded-full" href={"https://wiki.archlinux.org/"}>
+                                <SiWikipedia className="h-6 w-6" />
+                            </Link>
+                            <Link className="p-4 bg-rp-moon-overlay rounded-full" href={"https://archlinux.org/"}>
+                                <SiArchlinux className="h-6 w-6" />
+                            </Link>
+                        </div>
+                        <div className="flex flex-col items-center justify-items-center">
+                            <h3 className="items-center justify-items-center p-1">
+                                Repo List
+                            </h3>
                         </div>
                         <div className="flex-1 w-full overflow-y-auto">
                             <RepoList />
